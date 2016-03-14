d88573ad26c01ca135c7992c75563ff568e263dd
nikrb <nik_rb@yahoo.com>
2016-03-14 01:59:45 +0000

    initial commit

---
+++ LoginNode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginNode
+//
+//  Created by Nick Scott on 13/03/2016.
+//  Copyright © 2016 Nick Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
