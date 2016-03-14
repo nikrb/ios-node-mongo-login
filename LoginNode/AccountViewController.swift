557a18cd3881384856d1a709e257cf642e74874c
nikrb <nik_rb@yahoo.com>
2016-03-14 16:59:19 +0000

    setup account creation scene

---
+++ LoginNode/AccountViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AccountViewController.swift
+//  LoginNode
+//
+//  Created by Nick Scott on 14/03/2016.
+//  Copyright © 2016 Nick Scott. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func submitClick(sender: UIButton) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
